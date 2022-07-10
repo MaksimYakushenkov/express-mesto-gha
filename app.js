358b9e49987847e0a9f22999560fe0dcc83e90c9
MaksimYakushenkov <breakingwebnews@icloud.com>
2022-07-10 22:25:54 +0300

    feat: PW13 is ready to review

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 app.use((req, res, next) => {
@@ -27,5 +28,5 @@ app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
 app.listen(3000, () => {
-  console.log('Ссылка на сервер');
+  console.log('Сервер запущен');
 });
