409d0257ad2f6fc7c29883684cf895070dcac421
Maksim Yakushenkov <breakingwebnews@icloud.com>
2022-07-27 00:04:04 +0300

    feat: PW14 is ready to code review

--- routes/cards.js
+++ routes/cards.js
@@ -8,7 +8,7 @@ router.get('/', getCards);
 router.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().min(2),
+    link: Joi.string().required().pattern(/^https?:\/\/[www.]?\S/i),
   }),
 }), createCard);
 router.delete('/:cardId', celebrate({
--- routes/users.js
+++ routes/users.js
@@ -19,7 +19,7 @@ router.patch('/me', celebrate({
 }), updateUser);
 router.patch('/me/avatar', celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string().required(),
+    avatar: Joi.string().required().pattern(/^https?:\/\/[www.]?\S/i),
   }),
 }), updateAvatar);
 
